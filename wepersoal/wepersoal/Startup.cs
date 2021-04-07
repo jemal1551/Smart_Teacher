55186788c14896132e4ff43650db318921030974
JEMAL <JEMAL@DESKTOP-HB1M7DC>
2021-04-07 22:23:54 +0300

    Commited first

---
+++ wepersoal/wepersoal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wepersoal.Startup))]
+namespace wepersoal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
